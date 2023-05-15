51d1e35d6c66a42044a030dd1c238d36e84ea1e8
Jeff Mofor <allojeff444@gmail.com>
2023-05-15 06:22:27 -0700

    Added AUTHORS file

---
+++ setenv.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _setenv -  changes or adds an environment variable
+ * @name: variable name
+ * @value: new value
+ * @overwrite: describes if the operation should overwite
+ * any already declared environment variable or not
+ *
+ * Return: int
+ */
+int _setenv(const char *name, const char *value, int overwrite)
+{
+
