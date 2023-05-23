4dc7176e53e6749191833ea01dddb7bb1c08f1fc
Jeff Mofor <allojeff444@gmail.com>
2023-05-23 14:41:29 -0700

    Fit to betty

--- shell.c
+++ shell.c
@@ -132,45 +132,6 @@ int get_user_input(char **input)
 }
 
 /**
- * handle_built - handles built in commands
- * @args: list of command line args
- * @envp: environment variables
- * @argv: contains name of run file
- * @count: count
- * Return: 1 (found) 0 (not found)
- */
-int handle_built(char **args, char **envp, char **argv, int count)
-{
-
-	if (_strcmp(args[0], "exit"))
-	{
-		handle_exit(args, argv, envp, count);
-		return (1);
-	}
-	else if (_strcmp(args[0], "setenv"))
-	{
-		_setenv(args, envp, argv[0], count);
-		return (1);
-	}
-	else if (_strcmp(args[0], "unsetenv"))
-	{
-		_unsetenv(args, envp, argv[0], count);
-		return (1);
-	}
-	else if (_strcmp(args[0], "cd"))
-	{
-		handle_cd(args, envp, argv[0], count);
-		return (1);
-	}
-	else if (_strcmp(args[0], "env"))
-	{
-		print_env(args, envp, argv);
-		return (1);
-	}
-	return (0);
-}
-
-/**
  * free_args - free args
  * @args: args
  */
